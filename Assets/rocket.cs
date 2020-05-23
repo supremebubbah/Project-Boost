728ecd40337059e8388494b205bb9ec500b8437a
supremebubbah <kihobex26@hotmail.com>
2020-05-23 20:06:05 +0200

    añadiendo sonidos

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -1,62 +1,104 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class rocket : MonoBehaviour
 {
-    [SerializeField]float rcsThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathShip;
+    [SerializeField] AudioClip nextLevel;
+
+
     Rigidbody rigidBody;
-    AudioSource audioThruster;
+    AudioSource audioSource;
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        audioThruster = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state!= State.Alive) { return; }
+       
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("ok");
+                //Do nothing
                 break;
-            case "Fuel":
-                print("fuel");
+            case "Finish":
+                AccionesFinish();
                 break;
             default:
-                print("Dead");
+                AccionesMuerte();
                 break;
         }
     }
+    private void AccionesFinish()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(nextLevel);
+        Invoke("LoadNextScene", 1f);
+    }
+    private void AccionesMuerte()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathShip);
+        Invoke("LoadFirstLevel", 1f);
+    }
 
-    private void Thrust()
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToThrustInput()
     {
         float thrustThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up*mainThrust);
-
-            if (!audioThruster.isPlaying)
-            {
-                audioThruster.Play();
-            }
+            ApplyThrust();
         }
         else
         {
-            audioThruster.Stop();
+            audioSource.Stop();
         }
     }
-    private void Rotate()
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; // take manual control 
 
